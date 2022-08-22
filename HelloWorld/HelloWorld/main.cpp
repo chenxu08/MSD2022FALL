cde38d59183d605c82413ada172b019ddf821d28
chenxu08 <u1369630@utah.edu>
2022-08-22 12:14:31 -0600

    add 2+2

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by 徐晨 on 8/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << 2+2;
+    
+    return 0;
+}
