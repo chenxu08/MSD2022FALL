9f99c8a604e1da71d7718ae6bd7e7085e4019525
chenxu08 <u1369630@utah.edu>
2022-09-22 15:51:43 -0600

    by chenxu

---
+++ Day14/classvector/classvector/main.cpp
@@ -0,0 +1,74 @@
+//
+//  main.cpp
+//  classvector
+//
+//  Created by 徐晨 on 9/17/22.
+//
+
+#include <iostream>
+using namespace std;
+class myvector{
+private:
+    int* data;
+    int capacity;
+    int size;
+public:
+    myvector(){
+        data = new int[1];
+        capacity =1;
+        size = 0;
+}
+void freevector (){
+    delete [] data;
+}
+void growvector(int* data){
+    int *temp = new int[2*capacity];
+    for (int i=0; i < capacity; i++){
+        temp[i]= data[i];
+    }
+    delete [] data;
+    capacity *=2;
+    data = temp;
+    temp = nullptr;
+}
+void pushback(int value){
+    if (size==capacity){
+        growvector(data);
+    }
+    data[size]= value;
+    size=size+1;
+}
+void popback(){
+    size --;
+}
+int get(int index){
+    if (index <= size){
+        return data[index];}
+    else{cout<< "error"<<endl;return 0;}
+}
+void set( int index , int newvalue){
+    data[index]=newvalue;
+}
+int getsize(){return size;}
+int getcapacity(){return capacity;}
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    myvector v;
+    v.pushback(10);
+    v.pushback(15);
+    v.pushback(20);
+    
+    
+    v.popback();
+    cout<<v.get(2)<< endl;
+    cout<< v.getsize()<<endl;
+    cout<< v.getcapacity()<<endl;
+
+    for (int i = 0; i <=(v.getsize()) ;i++){
+        cout << v.get(i)<<" ";
+    }
+    cout<<endl;
+    
+    return 0;
+}
