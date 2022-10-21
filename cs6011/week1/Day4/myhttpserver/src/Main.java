204a54e0bd97b27d8828ed11d9a3eee79a2c300c
chenxu08 <u1369630@utah.edu>
2022-10-21 06:56:00 -0600

    1

--- cs6011/week1/Day4/myhttpserver/src/Main.java
+++ cs6011/week1/Day4/myhttpserver/src/Main.java
@@ -38,9 +38,16 @@ public class Main {
             fr.close();
             }
             else{
+                InputStream i = new FileInputStream("/Users/xuchen/myGithubRepo/cs6011/week1/Day4/myhttpserver/src/404.html");
+                BufferedReader fr =new BufferedReader(new InputStreamReader(i));
                 pw.println("HTTP/1.1 404 File Not Found");
                 pw.println("Content-Type: text/html;charset=utf-8");
                 pw.println("");
+                String c =null;
+                while((c=fr.readLine())!=null){
+                    pw.print(c);
+                }
+                fr.close();
                 pw.flush();
             }
             pw.close();
